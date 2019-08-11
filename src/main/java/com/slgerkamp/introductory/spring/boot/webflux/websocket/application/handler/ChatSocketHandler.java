a455de5c03968cff8a8e3b9d1b88296de38c18a8
hiroo takaku <slgerkamp@gmail.com>
2019-08-11 12:16:18 +0900

    first commit

---
+++ src/main/java/com/slgerkamp/introductory/spring/boot/webflux/websocket/application/handler/ChatSocketHandler.java
@@ -0,0 +1,6 @@
+package com.slgerkamp.introductory.spring.boot.webflux.websocket.application.handler;
+
+import org.springframework.web.reactive.socket.WebSocketHandler;
+
+public class ChatSocketHandler implements WebSocketHandler {
+}
